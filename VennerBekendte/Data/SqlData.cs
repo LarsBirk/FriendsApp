cd84fbaa3f7032d00d02e69979ff1d6a77baedcf
SeraNoxaEAMV <72008720+SeraNoxaEAMV@users.noreply.github.com>
2021-09-19 20:29:00 +0200

    nothing special

---
+++ VennerBekendte/Data/SqlData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VennerBekendte.Data
+{
+    class SqlData
+    {
+        /*
+         * move all data classes to this folder.
+        */
+    }
+}
